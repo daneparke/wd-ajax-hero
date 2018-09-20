9c0f6690042e893868e98681aea55068159b0ea4
daneparke <dapa8522@colorado.edu>
2018-09-20 11:04:55 -0600

    Final Version of Ajax Hero

--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
-(function() {
+(function () {
   'use strict';
 
   const movies = [];
 
-  const renderMovies = function() {
+  const renderMovies = function () {
     $('#listings').empty();
 
     for (const movie of movies) {
       const $col = $('<div>').addClass('col s6');
       const $card = $('<div>').addClass('card hoverable');
-      const $content = $('<div>').addClass('card-content center');
-      const $title = $('<h6>').addClass('card-title truncate');
+      const $content = $('<div>').addClass('card-Content center');
+      const $title = $('<h6>').addClass('card-Title truncate');
 
       $title.attr({
         'data-position': 'top',
-        'data-tooltip': movie.title
+        'data-tooltip': movie.Title
       });
 
-      $title.tooltip({ delay: 50 }).text(movie.title);
+      $title.tooltip({ delay: 50 }).text(movie.Title);
 
-      const $poster = $('<img>').addClass('poster');
+      const $poster = $('<img>').addClass('Poster');
 
       $poster.attr({
-        src: movie.poster,
-        alt: `${movie.poster} Poster`
+        src: movie.Poster,
+        alt: `${movie.Poster} Poster`
       });
 
       $content.append($title, $poster);
@@ -41,9 +41,9 @@
 
       const $modal = $('<div>').addClass('modal').attr('id', movie.id);
       const $modalContent = $('<div>').addClass('modal-content');
-      const $modalHeader = $('<h4>').text(movie.title);
-      const $movieYear = $('<h6>').text(`Released in ${movie.year}`);
-      const $modalText = $('<p>').text(movie.plot);
+      const $modalHeader = $('<h4>').text(movie.Title);
+      const $movieYear = $('<h6>').text(`Released in ${movie.Year}`);
+      const $modalText = $('<p>').text(movie.Plot);
 
       $modalContent.append($modalHeader, $movieYear, $modalText);
       $modal.append($modalContent);
@@ -55,6 +55,33 @@
       $('.modal-trigger').leanModal();
     }
   };
+  var input = document.querySelector('#search');
+  var search = document.querySelector('#submit');
+  const proxy = 'https://cors-anywhere.herokuapp.com/'
+
+  search.addEventListener('click', submitInput)
+
+  function submitInput(event) {
+    event.preventDefault()
+    fetch(proxy + 'http://omdb-api.now.sh/?s=' + encodeURI(input.value))
+      .then(function (response) {
+        return response.json()
+        // console.log(response.json)
+      })
+      .then(function (data) {
+        movies.push(data.Search[0])
+        console.log(movies)
+        renderMovies();
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+    // console.log(movies)
+    // renderMovies();
+
+  }
+
+
 
   // ADD YOUR CODE HERE
 })();
